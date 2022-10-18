5fe855784830831ed12c8075288e977e6754297f
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-10-18 10:02:16 +0100

    modify client configuration file with puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# Modifies the Client SSH configuration file
+include stdlib
+
+file_line { 'IdentityFile':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
+  replace => 'true',
+}
+
+file_lline { 'Password':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  line    => '   PasswordAuthentication no',
+  replace => 'true',
+}
