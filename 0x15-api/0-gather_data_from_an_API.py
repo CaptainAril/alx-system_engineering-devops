74978bbb8e597538c04d251fe107003851db9614
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-11-15 06:25:28 +0100

    use api to return employee todo list progress

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This script uses a REST API, given an employee ID, returns
+information about his/her TODO list progress."""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    id = int(argv[1])
+    index = id - 1
+    todo_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+        id)
+    user_url = 'https://jsonplaceholder.typicode.com/users'
+    r = requests.get(todo_url).json()
+    u = requests.get(user_url)
+    employee_name = u.json()[index].get('name')
+
+    total = 0
+    completed = 0
+    task_completed = []
+    for i in r:
+        total += 1
+        if i.get('completed') is True:
+            completed += 1
+            task_completed.append(i.get("title"))
+    print("Employee {} is done with task ({}/{}):".format(
+        employee_name, completed, total))
+    for task in task_completed:
+        print("\t {}".format(task))
