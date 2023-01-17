8edcb4539319e3d8774e1a532f67f118db1a2e83
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-01-17 03:12:50 +0100

    Export all to JSON

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-"""Returns infomation on user's task status,
+"""Returns infomation on all users task status,
 and esports to JSON format."""
 
 if __name__ == "__main__":
     import csv
     import json
     import requests
-    from sys import argv
 
-    id = argv[1]
-    user = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    user = "https://jsonplaceholder.typicode.com/users"
     u = requests.get(user).json()
-    username = u.get("username")
+    all_employees = {}
+    for user in u:
+        id = user.get('id')
+        username = user.get('username')
+        all_employees[id] = []
 
-    todo = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
-    r = requests.get(todo).json()
+        todo = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+        r = requests.get(todo).json()
 
-    user_status = {}
-    user_status[id] = []
-    for task in r:
-        title = task.get('title')
-        status = task.get('completed')
-        tk = {"task": title, 'completed': status, 'username': username}
-        user_status[id].append(tk)
-    with open('{}.json'.format(id), 'w') as f:
-        json.dump(user_status, f)
-{"2": [{"task": "suscipit repellat esse quibusdam voluptatem incidunt", "completed": false, "username": "Antonette"}, {"task": "distinctio vitae autem nihil ut molestias quo", "completed": true, "username": "Antonette"}, {"task": "et itaque necessitatibus maxime molestiae qui quas velit", "completed": false, "username": "Antonette"}, {"task": "adipisci non ad dicta qui amet quaerat doloribus ea", "completed": false, "username": "Antonette"}, {"task": "voluptas quo tenetur perspiciatis explicabo natus", "completed": true, "username": "Antonette"}, {"task": "aliquam aut quasi", "completed": true, "username": "Antonette"}, {"task": "veritatis pariatur delectus", "completed": true, "username": "Antonette"}, {"task": "nesciunt totam sit blanditiis sit", "completed": false, "username": "Antonette"}, {"task": "laborum aut in quam", "completed": false, "username": "Antonette"}, {"task": "nemo perspiciatis repellat ut dolor libero commodi blanditiis omnis", "completed": true, "username": "Antonette"}, {"task": "repudiandae totam in est sint facere fuga", "completed": false, "username": "Antonette"}, {"task": "earum doloribus ea doloremque quis", "completed": false, "username": "Antonette"}, {"task": "sint sit aut vero", "completed": false, "username": "Antonette"}, {"task": "porro aut necessitatibus eaque distinctio", "completed": false, "username": "Antonette"}, {"task": "repellendus veritatis molestias dicta incidunt", "completed": true, "username": "Antonette"}, {"task": "excepturi deleniti adipisci voluptatem et neque optio illum ad", "completed": true, "username": "Antonette"}, {"task": "sunt cum tempora", "completed": false, "username": "Antonette"}, {"task": "totam quia non", "completed": false, "username": "Antonette"}, {"task": "doloremque quibusdam asperiores libero corrupti illum qui omnis", "completed": false, "username": "Antonette"}, {"task": "totam atque quo nesciunt", "completed": true, "username": "Antonette"}]}
+        for task in r:
+            title = task.get('title')
+            status = task.get('completed')
+            tk = {'username': username, "task": title, 'completed': status}
+            all_employees[id].append(tk)
+
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(all_employees, f)
