4237a4b44594eaaeb297c9128172e6fd249325f7
CaptainAril <obeemmanuelifeoluw@gmail.com>
2023-02-09 12:22:11 +0100

    Sky is the limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,10 @@
 file { '/etc/default/nginx':
   content => 'ULIMIT="-n 5000"',
+  before  => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
 
